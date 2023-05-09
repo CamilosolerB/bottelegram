8f220eef0d6d3c1351dc1bc56f9d3dd17b198f99
CamilosolerB <caansobu2@gmail.com>
2023-05-08 19:19:39 -0500

    error packaje

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.use(express.json());
 app.use(require('./routes/index.routes'));
 //configuracion del puerto
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), ()=>{`En el puerto: ${app.get('port')}`})
+app.listen(app.get('port'), ()=>{console.log(`En el puerto: ${app.get('port')}`)})
