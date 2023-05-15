ff088e785d55a4b8378ace3a4ad80745cc93c535
CamilosolerB <caansobu2@gmail.com>
2023-05-14 19:18:17 -0500

    botsito

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -10,7 +10,12 @@ const axios = require('axios');
 bot.on('polling_error', (error)=>{
     console.log(error)
 })
-
+bot.onText(/\start/, (msg)=>{
+    const chat_id = msg.chat_id;
+    const message = "Bienvenido al proyecto de plantita feliz, en cuanto el esp este activado podras empezar a recibir las alertas de nuestra plantita, estate atento :D";
+    chatId.push(chat_id);
+    bot.sendMessage(chat_id,message);
+});
 controller.SayHello = async (req,res) =>{
     const {message} = req.body;
     for(let id = 0; id < chatId.length; j++){
